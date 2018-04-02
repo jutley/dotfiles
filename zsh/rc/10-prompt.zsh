0c4e3e4d076ac97c7210a05e92217ef001def143
Jake Utley <j.utley93@gmail.com>
2018-04-01 21:09:20 -0700

    Update prompt, rename bindings file

--- zsh/rc/10-prompt.zsh
+++ zsh/rc/10-prompt.zsh
@@ -1,10 +1,13 @@
 function ins-or-cmd() {
-  echo -n '%{$fg[white]%}'
+  echo -n '$fg_bold[white]|$reset_color ' # delimeter
+  echo -n '%U' # start underline
+  echo -n '$fg[white]'
   if [[ "$KEYMAP" == "main" ]]; then
     echo -n 'INS'
   else
     echo -n 'CMD'
   fi
+  echo '%u'
 }
 
 function date() {
@@ -12,9 +15,9 @@ function date() {
 }
 
 function update-custom-prompt() {
-  dir='%{$fg[cyan]%}%c'
-  line1="$(date) ${dir} $(git_prompt_info) -- $(ins-or-cmd)"
-  line2="${ret_status} %{$reset_color%}"
+  dir='$fg[cyan]%c'
+  line1="$(date) ${dir} $(git_prompt_info)$(ins-or-cmd)"
+  line2="${ret_status} $reset_color"
   ln=$'\n'
   export PROMPT="${ln}${line1}${ln}$line2"
 }
@@ -27,4 +30,3 @@ function zle-line-init zle-keymap-select() {
 zle -N zle-line-init
 zle -N zle-keymap-select
 update-custom-prompt
-

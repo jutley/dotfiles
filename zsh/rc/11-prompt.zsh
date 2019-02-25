6a9250d62943cc83cce98d40c85e401ae64fb380
Jake Utley <jutley@hiya.com>
2019-02-25 12:22:38 -0800

    Fix spacing

--- zsh/rc/11-prompt.zsh
+++ zsh/rc/11-prompt.zsh
@@ -16,7 +16,7 @@ function prompt-date() {
 
 function update-custom-prompt() {
   dir='%{$fg[cyan]%}%~'
-  line1="$(prompt-date) ${dir} $(git_prompt_info)  $(ins-or-cmd)"
+  line1="$(prompt-date) ${dir} $(git_prompt_info)$(ins-or-cmd)"
   line2="${ret_status} %{$reset_color%}"
   ln=$'\n'
   export PROMPT="${ln}${line1}${ln}$line2"

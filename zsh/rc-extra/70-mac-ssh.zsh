e876e3e3d9648e0273fe52265b667763874b4975
Jake Utley <j.utley93@gmail.com>
2024-06-02 10:06:43 -0700

    Configure git to use ssh key for signing via 1p

--- zsh/rc-extra/70-mac-ssh.zsh
+++ zsh/rc-extra/70-mac-ssh.zsh
@@ -1,1 +1,6 @@
 export SSH_AUTH_SOCK="$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+
+git config --global gpg.format ssh
+git config --global gpg.ssh.program "/Applications/1Password.app/Contents/MacOS/op-ssh-sign"
+git config --global commit.gpgsign true
+git config --global user.signingkey "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBsFYYdVPRO869veEiclFf8xGokNZpwjXBXh14SEn63P"

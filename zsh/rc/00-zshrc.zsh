8c9e03550176761bc60813321f6c650a38dee400
Jake Utley <j.utley93@gmail.com>
2024-06-02 10:37:01 -0700

    Fix some sourcing

--- zsh/rc/00-zshrc.zsh
+++ zsh/rc/00-zshrc.zsh
@@ -5,21 +5,21 @@ DISABLE_AUTO_UPDATE="true"
 
 plugins=(git kubectl docker)
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
-source $ZSH/oh-my-zsh.sh
-source $HOME/.fresh/build/shell.sh
+eval "$(brew shellenv)"
+source "$ZSH/oh-my-zsh.sh"
+source "$HOME/.fresh/build/shell.sh"
 
 autoload bashcompinit && bashcompinit
 complete -C "$(which aws_completer)" aws
 
 bindkey -v
 
-if [ -f ~/.exercism/exercism_completion.zsh ]; then
+if [[ -f ~/.exercism/exercism_completion.zsh ]]
+then
   source ~/.exercism/exercism_completion.zsh
 fi
 
 source "$HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-# source ~/awscli-ve/bin/activate
 
 eval "$(jenv init -)"
 eval "$(pyenv init -)"
